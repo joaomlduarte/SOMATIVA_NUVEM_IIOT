560d82f263b798939d4a6c42eb54994f5ae769c4
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-05-28 19:47:58 -0300

    vue instalado

---
+++ somativa-nuvem/src/App.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="https://img.freepik.com/premium-photo/iot-sensors-smart-agriculture-use-drones-plant-survey_909041-41.jpg?w=740" width="350" height="350" />
+
+    <div class="wrapper">
+      <HelloWorld msg="SOMATIVA NUVEM" />
+
+      <nav>
+        <RouterLink to="/">SOMATIVA NUVEM</RouterLink>
+        <RouterLink to="/about">SOMATIVA NUVEM</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
